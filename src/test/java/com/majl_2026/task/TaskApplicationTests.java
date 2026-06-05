db271a9fa31f5ff02f1cd61a5343518f3581d6e6
Mavis Javier <mavis.a.javier@gmail.com>
2026-06-05 10:13:37 -0500

    initial commit

---
+++ src/test/java/com/majl_2026/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majl_2026.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
