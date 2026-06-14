fd1f919ebd7ddd794f03faa04949f4b32ddaef57
Mavis Javier <mavis.a.javier@gmail.com>
2026-06-14 02:40:22 -0500

    setup config for UI test using vitest jest-dom

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,3 +1,5 @@
+// allows reference to test property provided by vitest
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -11,5 +13,12 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+  },
+  test: {
+    environment: 'jsdom',
+    // avoids the need to import `describe`, `it`, `expect` etc. in each test file
+    globals: true,
+    // runs inside each worker (test file) after globals are set up, so jest-dom can extend `expect`
+    setupFiles: ['./src/test/setup.ts'],
   },
 })
